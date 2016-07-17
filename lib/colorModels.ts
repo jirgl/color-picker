e3757d43ef4c199efc41983b0f31ccb8d4ec3406
Jakub Jirgl <jirgl.jakub@gmail.com>
2016-07-17 21:46:53 +0200

    added another convert functions

---
+++ lib/colorModels.ts
@@ -0,0 +1,13 @@
+export declare type hex = string;
+
+export declare type hsv = {
+    h: number;
+    s: number;
+    v: number;
+};
+
+export declare type rgb = {
+    r: number;
+    g: number;
+    b: number;
+};
