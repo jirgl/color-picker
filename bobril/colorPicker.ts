89d173d6223b3e65e65788cec43149d927e2e9c1
Jakub Jirgl <jirgl.jakub@gmail.com>
2016-07-19 21:54:59 +0200

    added draft of rgb channels

--- bobril/colorPicker.ts
+++ bobril/colorPicker.ts
@@ -1,46 +1,76 @@
 import * as b from 'bobril';
+import * as m from 'bobril-m';
 import { hex, hsv } from '../lib/colorModels';
 import * as colorConverter from '../lib/colorConverter';
 import { ColorPreview } from './components/colorPreview';
 import { HsvPreview } from './components/hsvPreview';
 import { ColorBar } from './components/colorBar';
 
-const defaultColor = { h: 115, s: 1, v: 1 };
+const defaultColor = { r: 0, g: 200, b: 0 };
 
 export interface IColorPickerData {
 }
 
 interface IColorPickerCtx extends b.IBobrilCtx {
     data: IColorPickerData;
-    hue: number;
+    red: number;
+    green: number;
+    blue: number;
     currentPreviewColor: hex;
     newPreviewColor: hex;
 }
 
 export const ColorPicker = b.createComponent<IColorPickerData>({
     init(ctx: IColorPickerCtx) {
-        ctx.hue = defaultColor.h;
-        ctx.currentPreviewColor = colorConverter.hsvToHex(defaultColor);
-        ctx.newPreviewColor = colorConverter.hsvToHex(defaultColor);
+        ctx.red = defaultColor.r;
+        ctx.green = defaultColor.g;
+        ctx.blue = defaultColor.b;
+        ctx.currentPreviewColor = colorConverter.rgbToHex(defaultColor);
+        ctx.newPreviewColor = colorConverter.rgbToHex(defaultColor);
     },
     render(ctx: IColorPickerCtx, me: b.IBobrilNode) {
         me.children = b.styledDiv([
             b.styledDiv(HsvPreview({
-                hsv: { h: ctx.hue, s: 1, v: 1 },
+                hsv: { h: colorConverter.rgbToHue({ r: ctx.red, g: ctx.green, b: ctx.blue }), s: 1, v: 1 },
                 onColorSelect: (hsv: hsv) => {
                     ctx.newPreviewColor = colorConverter.hsvToHex(hsv);
+                    const rgb = colorConverter.hsvToRgb(hsv);
+                    ctx.red = rgb.r;
+                    ctx.green = rgb.g;
+                    ctx.blue = rgb.b;
                     b.invalidate(ctx);
                 }
             }), { marginLeft: 5, marginRight: 5 }),
             b.styledDiv(ColorBar({
-                hue: ctx.hue,
+                hue: colorConverter.rgbToHue({ r: ctx.red, g: ctx.green, b: ctx.blue }),
                 onColorSelect: (hue: number) => {
-                    ctx.hue = hue;
+                    const rgb = colorConverter.hsvToRgb({ h: hue, s: 1, v: 1 });
+                    ctx.red = rgb.r;
+                    ctx.green = rgb.g;
+                    ctx.blue = rgb.b;
                     b.invalidate(ctx);
                 }
             }), { marginTop: 20 }),
             ColorPreview({ color: ctx.currentPreviewColor }),
-            ColorPreview({ color: ctx.newPreviewColor })
+            ColorPreview({ color: ctx.newPreviewColor }),
+            m.TextField({
+                labelText: 'red', value: ctx.red.toString(), onChange: (value) => {
+                    ctx.red = parseInt(value);
+                    b.invalidate(ctx);
+                }
+            }),
+            m.TextField({
+                labelText: 'green', value: ctx.green.toString(), onChange: (value) => {
+                    ctx.green = parseInt(value);
+                    b.invalidate(ctx);
+                }
+            }),
+            m.TextField({
+                labelText: 'blue', value: ctx.blue.toString(), onChange: (value) => {
+                    ctx.blue = parseInt(value);
+                    b.invalidate(ctx);
+                }
+            })
         ], { padding: 10 });
     }
 });
