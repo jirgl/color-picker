baba846b85cb6dd62b13648539536902cdb719a7
Jakub Jirgl <jirgl.jakub@gmail.com>
2016-07-28 20:32:03 +0200

    Add example of colorPicker with default color

--- bobril/index.ts
+++ bobril/index.ts
@@ -2,10 +2,17 @@ import * as b from 'bobril';
 import { ColorPicker } from './src/colorPicker';
 
 b.init(() => {
-    return b.styledDiv(ColorPicker({
-        color: '#ff00ff',
-        width: 300,
-        onClose: () => { },
-        onColorSelect: (color) => { }
-    }), { padding: 30 });
+    return b.styledDiv([
+        b.styledDiv(ColorPicker({
+            color: '#ff00ff',
+            width: 300,
+            onClose: () => { },
+            onColorSelect: (color) => { }
+        }), { padding: 30, flex: 1 }),
+        b.styledDiv(ColorPicker({
+            width: 300,
+            onClose: () => { },
+            onColorSelect: (color) => { }
+        }), { padding: 30, flex: 1 })
+    ], { display: 'flex' });
 });
